aee020cefffe9aba7fd8d14e528e6fc290bc8593
RAMEES K <161185637+rk897@users.noreply.github.com>
2026-01-14 20:20:58 +0530

    Initialize Express server with a success message

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('CI/CD Pipeline Deployed Successfully 🚀');
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
